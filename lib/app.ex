c99e389398046ab5e10f892d182c1efa6ddbd8c5
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-03-10 14:55:30 +0000

    autoload hack

--- lib/app.ex
+++ lib/app.ex
@@ -2,6 +2,7 @@ defmodule Saloon.App do
   use Application.Behaviour
   
   def start(_,_) do
+    lc x inlist :filelib.wildcard('ebin/*.beam'), do: :code.ensure_loaded(list_to_atom( Path.basename(Path.basename(x, '.beam')) ))
     env = Application.environment :saloon
     dispatch = :cowboy_router.compile env[:http_dispatch]
     :cowboy.start_http :http, 100, [port: env[:http_port]], [env: [dispatch: dispatch]]
--- lib/index.ex
+++ lib/index.ex
@@ -15,12 +15,14 @@ defmodule Saloon.Index do
     env = Application.environment :saloon
     {path, _} = :cowboy_req.path(req)
     [controller|_args] = String.split String.strip(path, ?/), "/"
+    IO.puts "Trying to convert #{env[:controller_prefix]}#{controller}#{env[:controller_postfix]} to existing atom..."
     try do 
       binary_to_existing_atom(<<
         env[:controller_prefix] :: binary, 
         controller :: binary,
         env[:controller_postfix] :: binary>>)
     catch _,_ ->
+      IO.puts "...failed"
       env[:c404]
     end
   end
