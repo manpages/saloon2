5295ba594fdefa27ce74586c0a2568777b569397
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-03-11 02:21:02 +0000

    add update and delete skeleton

--- lib/storage.ex
+++ lib/storage.ex
@@ -19,6 +19,19 @@ defmodule Saloon.Storage do
       options = [{:values, values} | options]
       compile(:insert, [:ignore, :into, :keys, :values], "INSERT ", options)
     end
+    
+    def update(table, set, where, options // []) do
+      options = [{:table, table} | options]
+      options = [{:set, set} | options]
+      options = [{:where, where} | options]
+      compile(:update, [:ignore, :table, :set, :where], "UPDATE ", options)
+    end
+
+    def delete(from, where, options // []) do
+      options = [{:from, from} | options]
+      options = [{:where, where} | options]
+      compile(:delete, [:from, :where, :limit], "DELETE FROM ", options)
+    end
 
     defp pool, do: Application.environment(:saloon)[:emysql_pool]
 
@@ -42,10 +55,20 @@ defmodule Saloon.Storage do
     defp compile!(:select, :group, _opt), do: nil
     defp compile!(:select, :order, _opt), do: nil
     defp compile!(:select, :limit, _opt), do: nil
+    
     defp compile!(:insert, :ignore, _opt), do: nil
     defp compile!(:insert, :into, _opt), do: nil
     defp compile!(:insert, :keys, _opt), do: nil
     defp compile!(:insert, :values, _opt), do: nil
+
+    defp compile!(:update, :ignore, _opt), do: nil
+    defp compile!(:update, :table, _opt), do: nil
+    defp compile!(:update, :set, _opt), do: nil
+    defp compile!(:update, :where, _opt), do: nil
+
+    defp compile!(:delete, :from, _opt), do: nil
+    defp compile!(:delete, :where, _opt), do: nil
+    defp compile!(:delete, :limit, _opt), do: nil
 
   end
 end
